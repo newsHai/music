8792ed0c23b7340a4eb20f2895efa7d860a3f436
newsHai <gongxiaohai1008@sina.com>
2018-01-10 18:01:32 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,14 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-
-Vue.config.productionTip = false
+// import router from './router'
+import 'common/stylus/index.styl'
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+    el: '#app',
+    render: h => h(App)
 })
