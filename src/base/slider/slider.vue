0c81b63023b7e0314a58f1100b667b841aaad6f8
newsHai <gongxiaohai1008@sina.com>
2018-01-11 23:38:13 +0800

    update

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="slider">
-      <div class="slider-group">
+  <div class="slider" ref="slider">
+      <div class="slider-group" ref="sliderGroup">
           <slot></slot>
       </div>
       <div class="dots"></div>
@@ -8,13 +8,55 @@
 </template>
 
 <script>
+import BScroll from 'better-scroll'
+import {addClass} from 'common/js/dom'
 export default {
+    props:{
+        loop:{
+            type:Boolean,
+            default:true
+        },
+        autoPlay:{
+            type:Boolean,
+            default:true
+        },
+        interval:{
+            type:Number,
+            default:4000
+        }
+    },
+    mounted(){
+        setTimeout(() => {
+            this._setSliderWidth();
+            this._initSlider();
+        },20)
+    },
+    methods:{
+       _setSliderWidth(){
+           this.children = this.$refs.sliderGroup.children;
+           let width=0;
+           let sliderWidth = this.$refs.slider.clientWidth;
+           for(let i=0;i<this.children.length;i++){
+               let child = this.children[i];
+               addClass(child, 'slider-item');
+               child.style.width = sliderWidth;
+               width += sliderWidth;
+           }
+           
+           if(this.loop){
+               width += 2*sliderWidth;
+           }
+           this.$refs.sliderGroup.style.width = width + 'px';
+       },
+       _initSlider(){
 
+       }
+    }
 }
 </script>
 
 <style scoped lang="stylus">
-    @import "common/stylus/variable"
+    @import "~common/stylus/variable"
     
     .slider
         min-height 1px
@@ -22,4 +64,17 @@ export default {
             position relative
             overflow hidden
             white-space nowrap
+            .slider-item
+                float left
+                box-sizing border-box
+                overflow hidden
+                text-align center
+                a
+                    display block
+                    width 100%
+                    overflow hidden
+                    text-decoration none
+                img
+                    display: block
+                    width: 100%
 </style>
